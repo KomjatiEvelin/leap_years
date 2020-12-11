b7dbe8adc7f81546b70f2ddc84b8eb326c6d9979
KomjatiEvelin <komjatievelin@gmail.com>
2020-12-11 09:46:05 +0100

    átlláthatóvá tétel vizsgálatok kiemelése külön

--- src/api.js
+++ src/api.js
@@ -3,10 +3,18 @@ const dummy_function = () => {
     return true;
 }
 
+const LEAP="LEAP";
+const NOT_LEAP="NOT LEAP";
 const isLeap=(year)=>{
 
-    return ((year%100===0)?(year%400===0):(year%4===0))?"LEAP":"NOT LEAP";
+    return ((isDivisibleBy100(year))?(isDivisibleBy400(year)):(isDivisibleBy4(year)))?LEAP:NOT_LEAP;
 }
+
+const isDivisibleBy100=(year)=>{return year%100===0};
+
+const isDivisibleBy400=(year)=>{return year%400===0};
+
+const isDivisibleBy4=(year)=>{return year%4===0};
 
 module.exports = {
     dummy_function : dummy_function,
